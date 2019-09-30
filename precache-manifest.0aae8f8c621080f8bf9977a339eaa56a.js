56d941e8a86531909b450af6ea11ea221aea033e
Marta Grus <mrs_marta@o2.pl>
2019-09-30 14:11:50 +0200

    Updates

---
+++ precache-manifest.0aae8f8c621080f8bf9977a339eaa56a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b2b6738488f45d0007db532178db19",
+    "url": "/my-games/index.html"
+  },
+  {
+    "revision": "52033417d314360d9fee",
+    "url": "/my-games/static/css/main.2f7fada5.chunk.css"
+  },
+  {
+    "revision": "86ad8e2682fb90cb99d8",
+    "url": "/my-games/static/js/2.d2eb95d0.chunk.js"
+  },
+  {
+    "revision": "52033417d314360d9fee",
+    "url": "/my-games/static/js/main.4b673a8d.chunk.js"
+  },
+  {
+    "revision": "71403cf61e2a0b5e7e1b",
+    "url": "/my-games/static/js/runtime~main.c903e4b9.js"
+  },
+  {
+    "revision": "ada0dd30560e3b1e3455e3f7b3b0b115",
+    "url": "/my-games/static/media/board.ada0dd30.php"
+  },
+  {
+    "revision": "5b37d03a8dd7be9d87c5c47a2fb26f09",
+    "url": "/my-games/static/media/gold.5b37d03a.jpg"
+  },
+  {
+    "revision": "776fe5759cfd2b59c89bfbccfef6f3d3",
+    "url": "/my-games/static/media/lasenter_personaluseonly-webfont.776fe575.woff2"
+  },
+  {
+    "revision": "d7764eeddff7e38e349fd1b218064131",
+    "url": "/my-games/static/media/lasenter_personaluseonly-webfont.d7764eed.woff"
+  },
+  {
+    "revision": "2f3c2c4f0c1f979cc73c353d4f3c1267",
+    "url": "/my-games/static/media/lights.2f3c2c4f.php"
+  },
+  {
+    "revision": "afa0d8704b5adf062afe24b9f3e52abd",
+    "url": "/my-games/static/media/lv.afa0d870.jpg"
+  },
+  {
+    "revision": "5e35d6818048e5d1a2ec5d23119c86be",
+    "url": "/my-games/static/media/neontubes2-webfont.5e35d681.woff2"
+  },
+  {
+    "revision": "e35f7a5eb38df9f46c79c8ec2e86245d",
+    "url": "/my-games/static/media/neontubes2-webfont.e35f7a5e.woff"
+  },
+  {
+    "revision": "a59aafee5b8be84ca5f3fc41a4518d62",
+    "url": "/my-games/static/media/o.a59aafee.jpg"
+  },
+  {
+    "revision": "4a41fc69d85c9728e473817b3dd90b01",
+    "url": "/my-games/static/media/tennis.4a41fc69.jpg"
+  },
+  {
+    "revision": "86a486837bf6f44288731de3f7093838",
+    "url": "/my-games/static/media/x.86a48683.jpg"
+  }
+]);
